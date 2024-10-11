6d56185ec982f1831af8871f285dfcfe4f4a4efb
Luis <x.luismoreno@gmail.com>
2024-10-11 14:58:57 -0400

    Update backend_install.sh
    updates to backend_install.sh

--- terraform/backend_install.sh
+++ terraform/backend_install.sh
@@ -5,3 +5,17 @@ sudo apt install -y git
 git clone https://github.com/YashMarmat/FullStack_Ecommerce_App.git
 
 # python 3.9
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt update
+sudo apt install python3.9 python3.9-venv python3.9-dev
+
+# python packages and start django
+cd FullStack_Ecommerce_App/backend
+
+python3.9 -m venv env
+
+source env/bin/activate
+
+pip install -r requirements.txt
+
+python manage.py runserver 0.0.0.0:8000
